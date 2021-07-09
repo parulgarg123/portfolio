5f3b95ebb692b6e641b484b4843ac9bf3c562869
Parul Garg <30907440+parulgarg123@users.noreply.github.com>
2021-07-09 14:33:41 +0530

    Basic skeleton for commands and portfolio structure.
    Signed-off-by: Parul Garg <garparul@amazon.com>

--- constants.js
+++ constants.js
@@ -1,6 +1,14 @@
-var constants = {
-    help: 'Use commands like \n>iam userName\n',
+const constants = {
+    HELP: 'Currently we support below mentioned commands: \n',
     GREETINGS: 'Welcome into the world of Parul Garg \nTry hitting help command.\n',
-    projects: 'projects',
+    PROJECTS: 'project',
+    EDUCATION: 'educations',
+    DETAILS: 'personal detail',
+    INTRODUCTION: 'introduction',
+    CONSTANT_DETAILS: 'Contact Details:' +
+        '\n > Phone Number : +91-99151-78377' +
+        '\n > email : parulg014@gmail.com' +
+        '\n',
 
-}
+    WHITELISTED_COMMANDS: ['details', 'projects', 'education', 'intro', 'contact'],
+};
