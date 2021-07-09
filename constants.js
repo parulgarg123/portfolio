38b7e5f3056c948e756558c047fc06c0aee77ae3
Parul Garg <garparul@amazon.com>
2021-07-09 13:47:37 +0530

    Initial changes

---
+++ constants.js
@@ -0,0 +1,6 @@
+var constants = {
+    help: 'Use commands like \n>iam userName\n',
+    GREETINGS: 'Welcome into the world of Parul Garg \nTry hitting help command.\n',
+    projects: 'projects',
+
+}
